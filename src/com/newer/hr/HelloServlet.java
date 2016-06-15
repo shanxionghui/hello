35dfae29655e727b82bb52a9da0b4af4c5fc3fc8
wtaon <wtaocs@126.com>
2016-06-15 14:38:15 +0800

    修改 Hello

--- src/com/newer/hr/HelloServlet.java
+++ src/com/newer/hr/HelloServlet.java
@@ -14,13 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HelloServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
